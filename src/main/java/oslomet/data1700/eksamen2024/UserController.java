203827692cec184b5ddf4a9aa8616b63d7d23898
sandersletvold <143513259+sandersletvold@users.noreply.github.com>
2024-06-17 23:11:21 +0200

    Fullført oppgave 1 og 2

---
+++ src/main/java/oslomet/data1700/eksamen2024/UserController.java
@@ -0,0 +1,12 @@
+package oslomet.data1700.eksamen2024;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @PostMapping("/saveUser")
+    public void saveUser() {
+
+    }
+}
---
+++ src/main/resources/static/script.js
@@ -0,0 +1,55 @@
+// Task 1 and 2
+function createUser() {
+    $("#createUserMessage").html("");
+    $("#firstnameError").html("");
+    $("#surnameError").html("");
+    $("#emailError").html("");
+    $("#phoneNumberError").html("");
+    $("#birthDateError").html("");
+
+    const user = {
+        firstName : $("#firstname").val(),
+        surName : $("#surname").val(),
+        phoneNumber : $("#phoneNumber").val(),
+        email : $("#email").val(),
+        birthDate : $("#birthDate").val()
+    };
+    const nameRegex = /^[a-zA-Z]{2,20}$/;
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const phoneNumberRegex = /^[0-9]{8}$/;
+
+    let validUser = true;
+
+    if (!nameRegex.test(user.firstName)) {
+        validUser = false;
+        $("#firstnameError").html("Invalid input for firstname");
+    }
+    if (!nameRegex.test(user.surName)) {
+        validUser = false;
+        $("#surnameError").html("Invalid input for surname");
+    }
+    if (!emailRegex.test(user.email)) {
+        validUser = false;
+        $("#emailError").html("Invalid input for email");
+    }
+    if (!phoneNumberRegex.test(user.phoneNumber)) {
+        validUser = false;
+        $("#phoneNumberError").html("Invalid input for phone number");
+    }
+    if (user.birthDate == null) {
+        validUser = false;
+        $("#birthDateError").html("Invalid input for birthdate");
+    }
+
+    if (validUser) {
+        $.post("/saveUser", user, function (){
+            $("#createUserMessage").html("User created successfully");
+            $("#firstname").val("");
+            $("#surname").val("");
+            $("#email").val("");
+            $("#phoneNumber").val("");
+            $("#birthDate").val("");
+            console.log(user);
+        });
+    }
+}
